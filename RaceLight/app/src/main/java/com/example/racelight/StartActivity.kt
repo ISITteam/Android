a401e5e2f98b9b46014008e90e8c43d45e53d0a5
dwikit <diwikstrom@gmail.com>
2020-05-29 16:13:32 -0700

    change to driver model and startActivity to add more props to driver and send to db

--- RaceLight/app/src/main/java/com/example/racelight/StartActivity.kt
+++ RaceLight/app/src/main/java/com/example/racelight/StartActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.start_page.*
+import java.text.DateFormat.getDateInstance
+import java.text.SimpleDateFormat
+import java.util.*
 
 class StartActivity: AppCompatActivity() {
 
@@ -21,13 +24,15 @@ class StartActivity: AppCompatActivity() {
     private fun driverTest(){
 
         val driverName = "Bobby"
+        val driverTime = .2
 
-        val ref = FirebaseDatabase.getInstance().getReference("test")
+        val driverDate = Date()
+
+        val ref = FirebaseDatabase.getInstance().getReference("Drivers")
 
         val dataID = ref.push().key
 
-
-        val driver = DriverTest(dataID, driverName)
+        val driver = DriverModel(dataID, driverName, driverTime, driverDate)
 
         //error occurred needed !! on dataID because of string, string ? mismatch
         ref.child(dataID!!).setValue(driver).addOnCompleteListener{
